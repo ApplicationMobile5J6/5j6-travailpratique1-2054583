cae46059c9b1b52012aae4e44f7d674f770f42b8
Ali tebbal <matricule@cstj.qc.ca>
2023-09-24 23:12:24 -0400

    acivité d'acceuil

--- app/src/main/java/com/example/alitebbaltp1/MainActivity.java
+++ app/src/main/java/com/example/alitebbaltp1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.alitebbaltp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        linearLayout.setBackgroundColor(getResources().getColor(R.color.grey));
+        Button btnReserver = findViewById(R.id.btn_reserver);
+        Button btnAfficher = findViewById(R.id.btn_afficherRes);
+        btnReserver.setBackgroundColor(getResources().getColor(R.color.brun));
+        btnAfficher.setBackgroundColor(getResources().getColor(R.color.brun));
+
     }
+
+
 }
